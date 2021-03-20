7d42d7dca7fa317a22d3778957a96817aa343308
AlGouvea <alvarohsgouvea@gmail.com>
2021-03-20 11:12:57 -0300

    Initiated reunion and agenda functions

--- lib.h
+++ lib.h
@@ -11,11 +11,19 @@ typedef struct agenda{
     int horarios[120];
 }a;
 
-a *newAgenda(){
+a *newAgenda(int op){
     a *newA;
 
-    for(int i = 0; i < 120; i++){
-        newA.horarios[i] = 0;
+    if(op == 1){
+        for(int i = 0; i < 120; i++){
+            newA->horarios[i] = 0;
+        }
+    }else{
+        FILE *f = fopen("agenda.txt", "r");
+        for(int i = 0; i < 120; i++){
+            fscanf(f, "%d", &newA->horarios[i]);
+        }
+        fclose(f);
     }
 
     return newA;
@@ -24,11 +32,11 @@ a *newAgenda(){
 void newReunion(int d, int s[], int e[]){
     r *newR = malloc(sizeof(r *));
 
-    newR.duration = d;
+    newR->duration = d;
 
-    newR.start[0] = s[0];
-    newR.start[1] = s[1];
+    newR->start[0] = s[0];
+    newR->start[1] = s[1];
 
-    newR.end[0] = e[0];
-    newR.end[0] = e[1];
+    newR->end[0] = e[0];
+    newR->end[0] = e[1];
 }
--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include "lib.h"
-int agenda = 0;
+int flag = -1;
+a *agenda;
 
 
 void menu(){
     printf("Welcome to Meet&Greed (Horario de Funcionamento: 08h as 18h)\n");
+
+    if(flag == -1){
+        int op;
+        printf("Digite o numero da sua escolha:\n0 -Usar a Agenda Existente\n1 -Criar nova Agenda");
+        flag = op;
+        agenda = newAgenda(op);
+    }
 }
 
 int main(){
